ce10489a6a9f51cfead173a824c7840b37e40ba8
Andy <antr900@gmail.com>
2020-03-23 21:56:58 -0700

    Added coffee icon

--- frontend/src/util/yelp_api.js
+++ frontend/src/util/yelp_api.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 const apiKey = require("../keys/keys").YELP_API_KEY;
 //${'https://cors-anywhere.herokuapp.com/'}
-
+//
 export const getYelpCafeById = (id) => {
-    return axios.get(`https://api.yelp.com/v3/businesses/${id}`, {
+    return axios.get(`${"https://cors-anywhere.herokuapp.com/"}https://api.yelp.com/v3/businesses/${id}`,
+      {
         headers: {
-            Authorization: `Bearer ${"UZittz7h5GXfqGN6CtGVeBd9Slxryw_l5kvsV8fRpS4D3jT9Zk0GnLWhvUsziHOoI52fl290Sg3JqCmJXPFxk3ooFdqTgSzja1AtBMQjTRQbXz2bDNEoc6TqZVBwXnYx"}`
-
-        },
-    })
+          Authorization: `Bearer ${"UZittz7h5GXfqGN6CtGVeBd9Slxryw_l5kvsV8fRpS4D3jT9Zk0GnLWhvUsziHOoI52fl290Sg3JqCmJXPFxk3ooFdqTgSzja1AtBMQjTRQbXz2bDNEoc6TqZVBwXnYx"}`
+        }
+      }
+    );
 
 };
 
