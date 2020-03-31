a411d5fe9ca551995eec9c2b0e401bf0d7712ee7
Andy <antr900@gmail.com>
2020-03-30 17:53:12 -0700

    Made backend changes

--- config/passport.js
+++ config/passport.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('./keys');
 
-//Testing
-
 const options = {};
 
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
