d7582d1f4acb7b071008d33c96204e62b8048cbb
Andy <antr900@gmail.com>
2020-03-30 17:27:15 -0700

    Adding test branch

--- config/passport.js
+++ config/passport.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const User = mongoose.model('users');
 const keys = require('./keys');
 
+//Testing
+
 const options = {};
 
 options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
