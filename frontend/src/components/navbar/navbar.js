67b2c27c90828b223e8a273b148649eee904c38e
Fei <feidyang@gmail.com>
2020-04-10 15:28:33 -0700

    double checking added favorites.scss change, changed to Studypal SF

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -167,7 +167,7 @@ class NavBar extends React.Component {
     render() {
         return(
             <div className="navbar">
-                <Link className="logo" to="/">Studypal</Link>
+                <Link className="logo" to="/">Studypal SF</Link>
                     {this.loggedOut()}
                     {this.loggedIn()}
             </div>
